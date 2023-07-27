ea53e7eaa5bbf8c4c3f1069b0fdfc97b5cd6dac4
Максим Прилепо <89262872286@yandex.ru>
2023-07-27 23:24:26 +0300

    хз8

--- backend/Foodgram/Foodgram/settings.py
+++ backend/Foodgram/Foodgram/settings.py
@@ -71,18 +71,6 @@ WSGI_APPLICATION = "Foodgram.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": os.getenv("POSTGRES_DB", "django"),
-#         "USER": os.getenv("POSTGRES_USER", "django"),
-#         "PASSWORD": os.getenv("POSTGRES_PASSWORD", ""),
-#         "HOST": os.getenv("DB_HOST", ""),
-#         "PORT": os.getenv("DB_PORT", 5432),
-#     }
-# }
-
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
