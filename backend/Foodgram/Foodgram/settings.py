225e239c1aa11b85bc35cbfd2ce31a0338d9b8c8
Максим Прилепо <89262872286@yandex.ru>
2023-07-29 15:42:27 +0300

    хз25

--- backend/Foodgram/Foodgram/settings.py
+++ backend/Foodgram/Foodgram/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'collected_static'
+STATIC_ROOT = BASE_DIR / 'backend_static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
